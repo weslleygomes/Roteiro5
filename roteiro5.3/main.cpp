74b3152ed8e00bf87562cb856e07064173c34171
weslleygomes <30627516+weslleygomes@users.noreply.github.com>
2017-08-29 12:46:47 -0300

    Update main.cpp

--- roteiro5.3/main.cpp
+++ roteiro5.3/main.cpp
@@ -1,14 +1,21 @@
 #include <iostream>
 #include "Empregado.h"
+#define TAM 10 //Defina o numero de Funcionários
 
 int main() {
-	Empregado funcionario;
-	funcionario.salariomensal();
-	if(funcionario.salario<0){
-		cout << "Salario invalido!";
+	int i;
+	Empregado funcionario[TAM];
+
+	for(i=0; i<TAM; i++){
+		funcionario[i].salariomensal();	
+			if(funcionario[i].salario<0){
+			cout << "Salario invalido!";
 		return 0;
+			}
 	}
-	funcionario.salarioanualAU();
-	
+	for(i=0; i<TAM; i++){
+		funcionario[i].salarioanualAU();
+	}
+
 	return 0;
 }
